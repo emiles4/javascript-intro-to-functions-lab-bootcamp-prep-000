83dbb40cc275b225be9b75bd5df56067ee59b2f3
Emily Miles <emiles4@gmail.com>
2018-05-16 16:25:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function logShout(string){
 }
 
 function sayHiToGrandma(string){
-  if (string === string.toLowerCase()) return "I can't hear you!";
+  if (string === string.toLowerCase()) return "I cant hear you!";
   else if (string === "I love you, Grandma.") return "I love you, too.";
   else if (string === string.toUpperCase()) return "YES INDEED!";
 }
