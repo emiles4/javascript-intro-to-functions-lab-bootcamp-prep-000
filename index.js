5c08bcfbfb3b7cc86483dea304e77cb9400e0d01
Emily Miles <emiles4@gmail.com>
2018-05-16 16:18:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,12 +20,14 @@ function logShout(string){
   console.log("HELLO".toUpperCase())
 }
 
-function sayHiToGrandma(string){
-  if("hello"===lowercase) {
- console.log("I cant hear you!")
-}
-  if("HELLO"===uppercase){
-  console.log("YES INDEED!")
-  }
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+  return "I can\'t hear you!";
+} 
+else if (string.toUpperCase() === string) {
+  return "YES INDEED!";
+} 
+else if (string === "I love you, Grandma.") {
+  return "I love you, too.";
   
 }
