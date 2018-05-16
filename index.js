74dd81db8535942b570696fb1d90989aca0a5141
Emily Miles <emiles4@gmail.com>
2018-05-16 16:16:44 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase
+
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logWhisper(string){
+  console.log("HELLO".toLowerCase())
+}
+function logShout(string){
+  console.log("HELLO".toUpperCase())
+}
+
+function sayHiToGrandma(string){
+  if("hello"===lowercase) {
+ console.log("I cant hear you!")
+}
+  if("HELLO"===uppercase){
+  console.log("YES INDEED!")
+  }
+  
+}
